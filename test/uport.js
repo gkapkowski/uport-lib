9cc3890f119bda01b802b1cead317ebb24013598
Joel Torstensson <oed3933@gmail.com>
2016-09-06 16:03:41 +0200

    Reverted back to using external testrpc for now

--- test/uport.js
+++ test/uport.js
@@ -2,9 +2,9 @@ import { assert } from 'chai'
 import Web3 from 'web3'
 import { Uport } from '../lib/index'
 import Autosigner from '../utils/autosigner'
-import startProviders from './providerUtil'
 import ProviderEngine from 'web3-provider-engine'
 import Web3Subprovider from 'web3-provider-engine/subproviders/web3'
+// import TestRPC from 'ethereumjs-testrpc'
 
 import testData from './testData.json'
 
@@ -22,38 +22,40 @@ describe('uport-lib integration tests', function () {
   before((done) => {
     global.navigator = {}
 
-    startProviders((err, provs) => {
+    // let testrpcProv = TestRPC.provider()
+    let testrpcProv = new Web3.providers.HttpProvider('http://localhost:8545')
+    web3 = new Web3(testrpcProv)
+    // Create Autosigner
+    Autosigner.load(testrpcProv, (err, as) => {
       if (err) { throw err }
-      web3 = new Web3(provs.web3Provider)
-      // Create Autosigner
-      Autosigner.load(provs.web3Provider, (err, as) => {
+      autosinger = as
+
+      web3.eth.getAccounts((err, accounts) => {
         if (err) { throw err }
-        autosinger = as
 
-        web3.eth.getAccounts((err, accounts) => {
-          if (err) { throw err }
+        // Create status contract
+        let statusContractABI = web3.eth.contract(testData.statusContractAbiData)
+        status = statusContractABI.new({
+          data: testData.statusContractBin,
+          from: accounts[0]
+        }, () => {})
+        // Send ether to Autosigner
+        web3.eth.sendTransaction({from: accounts[0], to: autosinger.address, value: web3.toWei(90)}, (e, r) => {
+          // Change provider
+          // Autosigner is a qrDisplay
+          // that automatically signs transactions
+          let uport = new Uport('Integration Tests', { qrDisplay: autosinger })
+          // Using the uportSubprovider be able to use testrpc programmatically.
+          // However TestRPC.provider() is causing problems with istanbul so it
+          // isn't used currently, but we still want to keep this strucure for later.
+          let uportSubprovider = uport.getUportSubprovider()
+          web3Provider = new ProviderEngine()
+          web3Provider.addProvider(uportSubprovider)
+          web3Provider.addProvider(new Web3Subprovider(testrpcProv))
+          web3Provider.start()
 
-          // Create status contract
-          let statusContractABI = web3.eth.contract(testData.statusContractAbiData)
-          status = statusContractABI.new({
-            data: testData.statusContractBin,
-            from: accounts[0]
-          }, () => {})
-          // Send ether to Autosigner
-          web3.eth.sendTransaction({from: accounts[0], to: autosinger.address, value: web3.toWei(90)}, (e, r) => {
-            // Change provider
-            // Autosigner is a qrDisplay
-            // that automatically signs transactions
-            let uport = new Uport('Integration Tests', { qrDisplay: autosinger })
-            let uportSubprovider = uport.getUportSubprovider()
-            web3Provider = new ProviderEngine()
-            web3Provider.addProvider(uportSubprovider)
-            web3Provider.addProvider(new Web3Subprovider(provs.web3Provider))
-            web3Provider.start()
-
-            web3.setProvider(web3Provider)
-            done()
-          })
+          web3.setProvider(web3Provider)
+          done()
         })
       })
     })
@@ -98,5 +100,10 @@ describe('uport-lib integration tests', function () {
         done()
       })
     })
+  })
+
+  after((done) => {
+    web3Provider.stop()
+    done()
   })
 })
